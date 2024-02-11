f9059a20440975ed5c055daf59bfd87b443b4d40
sajidg <sajid.amd@gmail.com>
2024-02-11 11:23:59 +0530

    added 3 models to the code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,12 @@
 
 print('adding all ML related code here')
+
+def model1:
+    print('code for model 1')
+
+def model2:
+    print('code for model 2')
+
+def model3:
+    print('code for model 1')
+
